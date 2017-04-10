83a4f8e9b221fbefb7728eb922e589e865ca9f09
Isaac Hepworth <isaac@stripe.com>
2017-04-09 22:25:18 -0600

    Blacklist @POTUS
    Dude gets congrats for every tweet. Not healthy.

--- congratsbot.py
+++ congratsbot.py
@@ -205,7 +205,7 @@ congrats_bot = ReplyAggregatorBot(
     search_terms=('congrats','congratulations'),
     threshold=5,
     response='congratulations!',
-    recipient_blacklist=recent_recipients + ["realDonaldTrump"],
+    recipient_blacklist=recent_recipients + ["realDonaldTrump"] + ["POTUS"],
     exclusion_heuristic=congrats_heuristic)
 
 congrats_bot.run()
