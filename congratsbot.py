5b624109ade0425599b2e004d350787d04c82686
Jed Christiansen <jedchristiansen@users.noreply.github.com>
2017-10-31 16:59:53 -0700

    Blacklist DJT Junior

--- congratsbot.py
+++ congratsbot.py
@@ -205,7 +205,7 @@ congrats_bot = ReplyAggregatorBot(
     search_terms=('congrats','congratulations'),
     threshold=5,
     response='congratulations!',
-    recipient_blacklist=recent_recipients + ["realDonaldTrump"] + ["POTUS"],
+    recipient_blacklist=recent_recipients + ["realDonaldTrump"] + ["POTUS"] + ["DonaldJTrumpJr"],
     exclusion_heuristic=congrats_heuristic)
 
 congrats_bot.run()
